3404300d4cfec06f798cb22c621fcf90f3cad80b
Taylan Karaman <taylan.karaman@gmail.com>
2017-10-04 14:56:55 +1100

    Initial commit

--- AcmeApp/Tests/Acme.BizTests/VendorTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Acme.Biz;
+using Acme.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -54,6 +55,83 @@ namespace Acme.Biz.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12);
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        public void PlaceOrder_3Parameters()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12" +
+                "\r\nDeliver By: 25/10/2018");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12,
+                new DateTimeOffset(2018, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)));
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        public void PlaceOrder_4Parameters()
+        {
+            // Arrange
+            var vendor = new Vendor();
+            var product = new Product(1, "Saw", "");
+            var expected = new OperationResult(true,
+                "Order from Acme, Inc\r\nProduct: Tools-1\r\nQuantity: 12" +
+                "\r\nDeliver By: 25/10/2018" +
+                "\r\nInstructions: Deliver to the reception");
+
+            // Act
+            var actual = vendor.PlaceOrder(product, 12,
+                new DateTimeOffset(2018, 10, 25, 0, 0, 0, new TimeSpan(-7, 0, 0)),
+                "Deliver to the reception");
+
+            // Assert
+            Assert.AreEqual(expected.Success, actual.Success);
+            Assert.AreEqual(expected.Message, actual.Message);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PlaceOrder_NullProduct_Exception()
+        {
+            // Arrange
+            var vendor = new Vendor();
+
+            // Act
+            var actual = vendor.PlaceOrder(null, 12);
+
+            // Assert
+            // Expected exception
+        }
+
+        [TestMethod()]
+        public void PlaceOrderTest1()
+        {
+
         }
     }
 }
